91d48c1791b52627c5b36c7b1fd5be5cb7e1fdd2
Emilian Cebuc <cebucemiliano@gmail.com>
2016-07-05 22:51:57 +0100

    Updated calculator program with improved visual cleanliness and comments in get() function

--- Sources/ch7/theCalculatorRevamped.cpp
+++ Sources/ch7/theCalculatorRevamped.cpp
@@ -31,9 +31,9 @@ Token_stream::Token_stream()
 	:full(false), buffer(0) {
 }
 Token Token_stream::get()
+	//read characters from cin and compose a Token
 {
-	if (full) { //do we already have a Token ready?
-		//remove token from buffer
+	if (full) { //check if we already have a Token ready
 		full = false;
 		return buffer;
 	}
@@ -43,16 +43,22 @@ Token Token_stream::get()
 
 	switch (ch)
 	{
-	case ';':	//for "print"
-	case 'q':	//for "quit"
-	case '(': case ')': case '+': case '-': case '*': case '/':
+	case print:
+	case quit:
+	case '(': 
+	case ')': 
+	case '+': 
+	case '-': 
+	case '*': 
+	case '/':
 		return Token(ch);	//let each character represent itself
-	case '.':
-	case '0': case '1': case '2': case '3': case '4': case '5': case '6': case '7': case '8': case '9':
+	case '.':				//a floating-point-literal can start with a dot
+	case '0': case '1': case '2': case '3': case '4': 
+	case '5': case '6': case '7': case '8': case '9':
 	{
 		cin.putback(ch);		//put digit back into the input stream
 		double val;
-		cin >> val;
+		cin >> val;				//read a floating-point number (the compiler knows it's a numeric value)
 		return Token(number, val);
 	}
 	default:
