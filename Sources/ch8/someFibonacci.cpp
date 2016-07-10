4e49f6b555de3ff7850b2efa35690c9c30fca2a9
Emilian Cebuc <cebucemiliano@gmail.com>
2016-07-10 23:31:13 +0100

    Modified fibonacci file, counts now till max capacity of an int variable, but still has previous normal calculation functions

--- Sources/ch8/someFibonacci.cpp
+++ Sources/ch8/someFibonacci.cpp
@@ -2,7 +2,7 @@
 
 void print(string s, vector<int>& v);
 void fibonacci(int x, int y, vector<int>& v, int n);
-
+void fibonacciMax(int x, int y, vector<int>& v);
 int main()
 try {
 	vector<int> fib(2, 0); //vector has start with at least 2 elements
@@ -10,8 +10,9 @@ try {
 	cout << "Insert the first and the second values to populate the vector.\n";
 	cout << "Then insert how many numbers of the Fibonacci string you want to add." << endl;
 	cin >> first >> second >> elementsNo;
-	fibonacci(first, second, fib, elementsNo);
-	print("this is the situation of the vector so far: ", fib);
+	//fibonacci(first, second, fib, elementsNo);
+	fibonacciMax(first, second, fib);
+	//print("this is the situation of the vector so far: ", fib);
 
 	return 0;
 }
@@ -45,5 +46,26 @@ void fibonacci(int x, int y, vector<int>& v, int n)
 	{
 		temp = v[i - 1] + v[i - 2];	//sum of previous 2 positions
 		v.push_back(temp);			//add another elements - the sum
+	}
+}
+
+void fibonacciMax(int x, int y, vector<int>& v)
+{
+	if (v.size() < 1)
+	{
+		error("vector has no elements in it");
+	}
+	int temp = 0;
+	v[0] = x;
+	v[1] = y;
+	for (int i = 2; ; ++i)
+	{
+		if (temp < 0)
+		{
+			break;
+		}
+		temp = v[i - 1] + v[i - 2];	//sum of previous 2 positions
+		v.push_back(temp);			//add another elements - the sum
+		cout << temp << endl;
 	}
 }
